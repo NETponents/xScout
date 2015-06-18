dd89e66467d41e742eef9f2a06fe995b1b07c255
Joshua Zenn <wordman05@gmail.com>
2015-06-18 17:22:19 -0400

    Update api.rb

--- inc/api.rb
+++ inc/api.rb
@@ -1,9 +1,10 @@
 module API
   require_relative "pagevars"
+  def API.processCMD()
+    return "The API is online"
+  end
   def API.processCMD(cmd)
-    if cmd == ""
-      return "The API is online"
-    elsif cmd == "version"
+    if cmd == "version"
       return Pagevars.setVars("CIbuild")
     else
       return "404 API call not found"
