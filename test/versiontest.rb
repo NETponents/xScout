3f0523a237657f4285c78311fce867d273cb4189
Joshua Zenn <wordman05@gmail.com>
2015-06-18 12:31:30 -0400

    Update versiontest.rb

--- test/versiontest.rb
+++ test/versiontest.rb
@@ -3,6 +3,12 @@ require_relative "../inc/builddata"
 require "test/unit"
 
 class TestVersion < Test::Unit::TestCase
+  def test_VersionCommitString
+    assert_not_equal(nil, CIgetcommit())
+  end
+  def test_VersionBuildString
+    assert_not_equal(nil, CIgetbuild())
+  end
   def test_VersionString
     assert_not_equal(nil, getCIstring())
   end
