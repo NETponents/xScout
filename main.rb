7d40a06eae76c94288413d456cf852ddff33c57c
Joshua Zenn <wordman05@gmail.com>
2015-06-18 14:38:41 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -3,16 +3,16 @@ require 'rubygems'
 require 'sinatra'
 require 'slim'
 require_relative 'inc/pagevars'
-include 'pagevars'
+#include 'pagevars'
 
 set :port, ENV['PORT'] || 8080
 set :bind, ENV['IP'] || '0.0.0.0'
 
 get '/' do
-  pagevars::setVars("Home")
+  pagevars.setVars("Home")
   slim :home
 end
 get '/pricing' do
-  pagevars::setVars("Pricing")
+  pagevars.setVars("Pricing")
   slim :homepricing
 end
