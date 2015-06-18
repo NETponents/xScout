27190055a4a969ddc9d75bccbd111e14f79646fd
Joshua Zenn <wordman05@gmail.com>
2015-06-18 10:17:28 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -2,10 +2,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'slim'
+require_relative 'inc/builddata'
 
 set :port, ENV['PORT'] || 8080
 set :bind, ENV['IP'] || '0.0.0.0'
 
 get '/' do
+  @PageTitle = "Home"
+  @TRAVISBUILDNUMBER = getCIstring()
   slim :newhome
 end
